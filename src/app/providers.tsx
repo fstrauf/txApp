e52bc5753a1147546a82eaf60ac2fce8bc38238a
fstrauf <f.strauf@gmail.com>
2025-06-29 19:55:49 +1200

    removed survey

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,14 +2,12 @@
 
 import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
 import PostHogProviderWrapper from '@/components/PostHogProvider';
-import PostSignupSurvey from '@/components/shared/PostSignupSurvey';
 
 export function SessionProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextAuthSessionProvider>
       <PostHogProviderWrapper>
         {children}
-        <PostSignupSurvey />
       </PostHogProviderWrapper>
     </NextAuthSessionProvider>
   );
